872d01b92439f3367a2e22d4323d9e504c98cdac
Alysia <alysia@bench.co>
2023-07-07 10:25:10 -0700

    :shower: Clean: run dependabot daily

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  coverageDirectory: 'reports',
-  coverageReporters: ['text', 'cobertura'],
-  preset: 'ts-jest',
-  reporters: ['default', ['jest-junit', { outputDirectory: 'reports', outputName: 'jest.xml' }]],
-  testEnvironment: 'jsdom',
-};
