b36f4dfcf5d010375f9b4bfc7194deedf0b02875
Alysia <alysia@bench.co>
2023-07-08 15:01:28 -0700

    :shower: Clean: fix lint error

--- src/ui/atoms/colors.ts
+++ src/ui/atoms/colors.ts
@@ -2,6 +2,8 @@ const colors = {
   galaxy: '#201B28',
   grape: '#6E00FF',
   iron: '#C6CDD6',
+  king: '#6851FF',
+  lavender: '#AD77FF',
   leaf: '#23D5AB',
   night: '#222125',
   onyx: '#2C2D31',
@@ -9,10 +11,8 @@ const colors = {
   slate: '#F6F6F6',
   snow: '#F2F4F5',
   sunrise: '#DC90FF',
-  white: '#FFFFFF',
-  lavender: '#AD77FF',
-  king: '#6851FF',
   whale: '#7CBFFF',
+  white: '#FFFFFF',
 };
 
 export default colors;
